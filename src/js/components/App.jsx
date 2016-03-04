3625d98c5173bde39f776fae44808c6349359801
José Cunha Fonte <067-jose-p-font@sapo.corppt.com>
2016-03-04 12:58:14 +0000

    Insert goat phrases

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -17,55 +17,55 @@ const App = React.createClass({
                 {
                     url: 'http://funny-wall.com/wp-content/uploads/Funny-Goat-Drinking-Beer.jpg',
                     displayName: 'Adele',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'I feel Braaaaad',
                     published: 1167609600
                 },
                 {
                     url: 'http://www.ourhenhouse.org/wp-content/uploads/2013/02/goat_peaceful_prairie.jpg',
                     displayName: 'Agatha',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'Shit just goat serious',
                     published: 1230768000
                 },
                 {
                     url: 'https://handmadelives.files.wordpress.com/2013/11/goat-best.jpg',
                     displayName: 'Alexis',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'Calm the fuck down I goat this',
                     published: 1262304000
                 },
                 {
                     url: 'http://www.ourhenhouse.org/wp-content/uploads/2013/06/goat_farm_sanctuary.jpg',
                     displayName: 'Dimitra',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'Hey man Goat any grass?',
                     published: 1325376000
                 },
                 {
                     url: 'http://creepy.blogs.com/photos/uncategorized/2007/11/27/goat.jpg',
                     displayName: 'Amelia',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'I don’t always eat everything Oh wait, yes i do',
                     published: 1355376000
                 },
                 {
                     url: 'http://forum.unity3d.com/attachments/526d8bb21605fb164400000d-jpg.93914/',
                     displayName: 'Erika',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'You have goat To be kidding me',
                     published: 1385376000
                 },
                 {
                     url: 'http://randomoverload.org/wp-content/uploads/2016/01/5895hE9398D3F.jpg',
                     displayName: 'Louise',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'Here we Goat Again…',
                     published: 1425376000
                 },
                 {
                     url: 'http://cdn-img.people.com/emstag/i/2015/pets/news/150323/goat-800.jpg',
                     displayName: 'Marion',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'Row row row Your goat',
                     published: 1395376000
                 },
                 {
                     url: 'http://www.hobbyfarms.com/images/editorial-blog/cyndi-cady/choosing-animals_800.jpg',
                     displayName: 'Nora',
-                    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                    description: 'For goat’s sake, that’s enough',
                     published: 1335376000
                 }
             ]
